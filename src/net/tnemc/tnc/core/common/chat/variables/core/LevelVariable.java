f4fafbfef3965d90b569039d9a12b3963efa756d
Daniel V <daniel.viddy@gmail.com>
2018-10-08 10:05:34 -0400

    Core Chat Variables.

---
+++ src/net/tnemc/tnc/core/common/chat/variables/core/LevelVariable.java
@@ -0,0 +1,25 @@
+package net.tnemc.tnc.core.common.chat.variables.core;
+
+import net.tnemc.tnc.core.common.chat.ChatVariable;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by creatorfromhell.
+ *
+ * The New Chat Minecraft Server Plugin
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0
+ * International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/
+ * or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ */
+public class LevelVariable extends ChatVariable {
+  @Override
+  public String name() {
+    return "$level";
+  }
+
+  @Override
+  public String parse(Player player, String message) {
+    return player.getLevel() + "";
+  }
+}
